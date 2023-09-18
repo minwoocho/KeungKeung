db18d9b20e173a8b6739139213a544ee1854c722
Jerry <dong.lee@lgcns.com>
2023-09-18 14:30:13 +0900

    .

--- app.ts
+++ app.ts
@@ -1,38 +1,41 @@
-import express from 'express';
-import pug from 'pug';
-import fs from 'fs';
-import mysql from 'mysql'; // mysql 모듈을 불러옵니다.
+import express from "express";
+import pug from "pug";
+import mysql from "mysql"; // mysql 모듈을 불러옵니다.
 const app = express();
 const port: number = 3000;
 
 // 커넥션을 정의합니다.
 // RDS Console 에서 본인이 설정한 값을 입력해주세요.
 const connection = mysql.createConnection({
-  host: 'keung-rds.colgpa78bwcs.ap-northeast-2.rds.amazonaws.com',
-  user: 'admin',
-  password: '!Dkagh00',
-  database: 'keung',
+  host: "keung-rds.colgpa78bwcs.ap-northeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "!Dkagh00",
+  database: "keung",
 });
 
-app.set('view engine', 'pug');
-app.use(express.static('public'));
-app.use(express.static('src'));
+app.set("view engine", "pug");
+app.use(express.static("public"));
+app.use(express.static("src"));
 
-const mockUserId = '00003';
+const mockUserId: string = "00003";
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/user', (req, res) => {
+app.get("/add-review", (req, res) => {
+  res.render("add-review");
+});
+
+app.get("/user", (req, res) => {
   const userId = mockUserId;
   connection.connect(function () {
     try {
       connection.query(
-        'SELECT NAME FROM TB_USER_MASTER WHERE USER_ID = ?',
+        "SELECT NAME FROM TB_USER_MASTER WHERE USER_ID = ?",
         [userId],
-        function (err, rows, fields) {
-          res.send(rows[0].NAME + '님! 어떤 식당을 찾으시나요?');
+        function (err, rows: any[], fields) {
+          res.send(rows[0].NAME + "님! 어떤 식당을 찾으시나요?");
         }
       );
     } catch (e) {
@@ -41,20 +44,17 @@ app.get('/user', (req, res) => {
   });
 });
 
-app.get('/add-review', (req, res) => {
-  res.render('add-review')
-});
-
-app.get('/review', (req, res) => {
-  connection.connect(function() {  
-    try{
+app.get("/reviews", (req, res) => {
+  connection.connect(function () {
+    try {
       connection.query(
-        "SELECT REVIEW_ID AS cardId FROM TB_REVIEW_MASTER ORDER BY REVIEW_ID DESC", function(err, rows: any[], fields) {
-
-          let template = pug.compileFile('views/components/card-layout.pug');
-          res.send(template({datas: rows}));
-      });
-    }catch (e){
+        "SELECT REVIEW_ID AS cardId FROM TB_REVIEW_MASTER ORDER BY REVIEW_ID DESC",
+        function (err, rows: any[], fields) {
+          let template = pug.compileFile("views/components/card-layout.pug");
+          res.send(template({ datas: rows }));
+        }
+      );
+    } catch (e) {
       console.log(e);
     }
   });
@@ -62,45 +62,75 @@ app.get('/review', (req, res) => {
 
 app.get("/review/:id", (req, res) => {
   const { id } = req.params;
-  connection.connect(function() {  
-    try{
+  connection.connect(function () {
+    try {
       connection.query(
-      `SELECT
-        TRM.REVIEW_ID
-        ,TRM.REVIEW_CONTENT as review
-        ,TSM.STORE_NAME as storeName
-        ,'/assets/photos/mock-picture.png' as photo
-        ,TUM.NAME as userName
-      FROM TB_REVIEW_MASTER TRM 
-      LEFT OUTER JOIN TB_STORE_MASTER TSM ON TRM.STORE_ID  = TSM.STORE_ID  
-      LEFT OUTER JOIN TB_IMAGE_MASTER TIM  ON TRM.REVIEW_ID  = TIM.REVIEW_ID
-      LEFT OUTER JOIN TB_USER_MASTER TUM ON TRM.USER_ID  = TUM.USER_ID
-      WHERE 1=1
-      AND TRM.REVIEW_ID = ?`,[id], function(err, rows: any[], fields) {
+        `
+        SELECT TRM.REVIEW_ID as reviewId
+              ,TRM.REVIEW_CONTENT as review
+              ,TSM.STORE_NAME as storeName
+              ,'/assets/photos/mock-picture.png' as photo
+              ,TUM.NAME as userName
+         FROM TB_REVIEW_MASTER TRM 
+         LEFT OUTER JOIN TB_STORE_MASTER TSM ON TRM.STORE_ID  = TSM.STORE_ID  
+         LEFT OUTER JOIN TB_IMAGE_MASTER TIM  ON TRM.REVIEW_ID  = TIM.REVIEW_ID
+         LEFT OUTER JOIN TB_USER_MASTER TUM ON TRM.USER_ID  = TUM.USER_ID
+        WHERE 1=1
+          AND TRM.REVIEW_ID = ?
+      `,
+        [id],
+        function (err, rows: any[], fields) {
+          const data = rows[0];
 
-      const data = rows[0];
-
-      let template = pug.compileFile("views/components/card.pug");
-      res.send(template({ data }));
-      });
+          let template = pug.compileFile("views/components/card.pug");
+          res.send(template({ data }));
+        }
+      );
     } catch (e) {
       console.log(e);
     }
   });
 });
 
-app.get("/tagdetail", (req, res) => {
-  try{
+app.get("/review/tags/:id", (req, res) => {
+  const { id } = req.params;
+  try {
     connection.query(
-      "SELECT TAG_ID as tagId, TAG_NAME as tagName, 'main' as type FROM TB_TAG_MASTER", function(err, rows: any[], fields) {
-        let template = pug.compileFile("views/components/tag-select.pug");      
+      `
+      SELECT TRT.TAG_ID as tagId
+            ,TTM.TAG_NAME as tagName
+            ,'card' as type 
+        FROM TB_REVIEW_TAG TRT
+        LEFT OUTER JOIN TB_TAG_MASTER TTM ON TRT.TAG_ID = TTM.TAG_ID 
+       WHERE TRT.REVIEW_ID = ?
+      `,
+      [id],
+      function (err, rows: any[], fields) {
+        let template = pug.compileFile("views/components/tag-select.pug");
         res.send(template({ tags: rows }));
-    });
-  }catch (e){
+      }
+    );
+  } catch (e) {
     console.log(e);
   }
 });
 
+app.get("/tagdetail", (req, res) => {
+  try {
+    connection.query(
+      `SELECT TAG_ID as tagId
+             ,TAG_NAME as tagName
+             ,'main' as type
+         FROM TB_TAG_MASTER`,
+      function (err, rows: any[], fields) {
+        let template = pug.compileFile("views/components/tag-select.pug");
+        res.send(template({ tags: rows }));
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
