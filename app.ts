7cf18318bcde087e623fa1932d5ae53e05d383cc
Jerry <dong.lee@lgcns.com>
2023-09-27 08:36:22 +0900

    코드가 이뻐졌어요!

--- app.ts
+++ app.ts
@@ -1,263 +1,224 @@
-import express from "express";
-import mysql, { MysqlError } from "mysql"; // mysql 모듈을 불러옵니다.
-import mybatisMapprer from "mybatis-mapper";
+import express, { NextFunction, Request, Response } from "express";
+import mysql, { RowDataPacket, FieldPacket, ResultSetHeader } from "mysql2/promise"; // mysql 모듈을 불러옵니다.
+import mybatisMapprer, { Params } from "mybatis-mapper";
 import bodyParser from "body-parser";
 import multer from "multer";
 
+require("dotenv").config();
+
 const app = express();
+app.set("view engine", "pug");
+
 const port = 3000;
+const logging: false | "debug" | "info" | "error" = false;
 
 const mockUserId = "00002";
 
-// 커넥션을 정의합니다.
-const connection = mysql.createConnection({
-  host: "keung-rds.colgpa78bwcs.ap-northeast-2.rds.amazonaws.com",
-  user: "admin",
-  password: "!Dkagh00",
-  database: "keung",
-});
-mybatisMapprer.createMapper(["src/sql/mapper.xml"]);
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("에러 발생:", err);
+  res.status(500).send("서버 에러 발생");
+  next(err);
+};
 
-app.set("view engine", "pug");
+app.use(errorHandler);
 app.use(express.static("public"));
 app.use(express.static("src"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(multer({ storage: multer.memoryStorage() }).array("img"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(`*===============================*`);
-  console.log(`==         Keung Keung         ==`);
-  console.log(`*===============================*`);
-});
-
-/**
- * 메인페이지
- */
-app.get("/", (req, res) => {
-  app.locals.userId = mockUserId;
-  app.locals.tagIds = new Set();
-  console.log("login User : " + app.locals.userId);
-  res.render("index");
-});
-
-app.get("/main/user", (req, res) => {
-  const userId = app.locals.userId;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "main/user",
-    { userId },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
-    res.send(rows[0].NAME + "님! 어떤 식당을 찾으시나요?");
+const start = async () => {
+  // 커넥션을 정의합니다.
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
-});
 
-app.get("/main/tags", (req, res) => {
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "main/tags",
-    {},
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  const select = (sql: string, val?: string) => {
+    logging && console.log(sql);
+    return connection.query(sql, val) as Promise<[RowDataPacket[], FieldPacket[]]>;
+  };
+
+  const insert = (sql: string, val?: string) => {
+    logging && console.log(sql);
+    return connection.query(sql, val) as Promise<[ResultSetHeader, FieldPacket[]]>;
+  };
+
+  const remove = (sql: string, val?: string) => {
+    logging && console.log(sql);
+    return connection.query(sql, val) as Promise<[ResultSetHeader, FieldPacket[]]>;
+  };
+
+  // 마이바티스 매퍼 정의
+  mybatisMapprer.createMapper(["src/sql/mapper.xml"]);
+
+  const bindSQL = (id: string, param?: Params) =>
+    mybatisMapprer.getStatement("app", id, param, {
+      language: "sql",
+      indent: "  ",
+    });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log(`*===============================*`);
+    console.log(`==         Keung Keung         ==`);
+    console.log(`*===============================*`);
+    console.log(process.env.TESTTT);
+  });
+
+  /**
+   * 메인페이지
+   */
+  app.get("/", (req, res) => {
+    app.locals.userId = mockUserId;
+    app.locals.tagIds = new Set();
+    console.log("login User : " + app.locals.userId);
+    res.render("index");
+  });
+
+  app.get("/main/user", async (req, res) => {
+    const userId = app.locals.userId;
+    const sql = bindSQL("main/user", { userId });
+    const [rows, fields] = await select(sql);
+    res.send(rows[0].name + "님! 어떤 식당을 찾으시나요?");
+  });
+
+  app.get("/main/tags", async (req, res) => {
+    const sql = bindSQL("main/tags");
+    const [rows, fields] = await select(sql);
     res.render("components/tag-select", { tags: rows });
   });
-});
 
-app.get("/main/cards", (req, res) => {
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "main/cards",
-    {},
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  app.get("/main/cards", async (req, res) => {
+    const sql = bindSQL("main/cards");
+    const [rows, fields] = await select(sql);
     res.render("components/card-layout", { datas: rows });
   });
-});
 
-app.get("/cards/:tagId", (req, res) => {
-  const { tagId } = req.params;
-  const tagIds: Set<string> = app.locals.tagIds;
+  app.get("/cards/:tagId", async (req, res) => {
+    const { tagId } = req.params;
+    const tagIds: Set<string> = app.locals.tagIds;
 
-  tagIds.has(tagId) ? tagIds.delete(tagId) : tagIds.add(tagId);
+    tagIds.has(tagId) ? tagIds.delete(tagId) : tagIds.add(tagId);
 
-  const sqlId = tagIds.size > 0 ? "main/cards/tagId" : "main/cards";
+    const sqlId = tagIds.size > 0 ? "main/cards/tagId" : "main/cards";
 
-  const query = mybatisMapprer.getStatement(
-    "app",
-    sqlId,
-    { tagIds: Array.from(tagIds) },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+    const sql = bindSQL(sqlId, { tagIds: Array.from(tagIds) });
+    const [rows, fields] = await select(sql);
     res.render("components/card-layout", { datas: rows });
   });
-});
 
-app.get("/card/:id", (req, res) => {
-  const { id } = req.params;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "card/id",
-    { id },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  app.get("/card/:id", async (req, res) => {
+    const { id } = req.params;
+    const sql = bindSQL("card/id", { id });
+    const [rows, fields] = await select(sql);
     res.render("components/card", { data: rows[0] });
   });
-});
 
-app.get("/card/images/:id", (req, res) => {
-  const { id } = req.params;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "card/images/id",
-    { id },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  app.get("/card/images/:id", async (req, res) => {
+    const { id } = req.params;
+    const sql = bindSQL("card/images/id", { id });
+    const [rows, fields] = await select(sql);
+
     if (rows.length === 0) {
       console.error("Image not found");
       res.status(404).send("Image not found");
       return;
     }
     const imageBuffer = rows[0].imageData;
-    console.log(imageBuffer);
     res.writeHead(200, {
       "Content-Type": "image/jpeg", // 이미지 형식에 따라 변경
       "Content-Length": imageBuffer.length,
     });
     res.end(imageBuffer);
   });
-});
 
-app.get("/card/tags/:id", (req, res) => {
-  const userId = app.locals.userId;
-  const { id } = req.params;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "card/tags/id",
-    { id, userId },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  app.get("/card/tags/:id", async (req, res) => {
+    const userId = app.locals.userId;
+    const { id } = req.params;
+    const sql = bindSQL("card/tags/id", { id, userId });
+    const [rows, fields] = await select(sql);
     res.render("components/tag-select", { tags: rows });
   });
-});
 
-app.put("/like", (req, res) => {
-  const userId = app.locals.userId;
-  const { reviewId, tagId } = req.body;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "like",
-    { userId, reviewId, tagId },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query);
-  res.send();
-});
+  app.put("/like", async (req, res) => {
+    const userId = app.locals.userId;
+    const { reviewId, tagId } = req.body;
+    const sql = bindSQL("like", { userId, reviewId, tagId });
+    const [result] = await insert(sql);
+    res.send();
+  });
 
-app.delete("/unlike", (req, res) => {
-  const userId = app.locals.userId;
-  const { reviewId, tagId } = req.body;
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "unlike",
-    { userId, reviewId, tagId },
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query);
-  res.send();
-});
+  app.delete("/unlike", async (req, res) => {
+    const userId = app.locals.userId;
+    const { reviewId, tagId } = req.body;
+    const sql = bindSQL("unlike", { userId, reviewId, tagId });
+    const [result] = await remove(sql);
+    res.send();
+  });
 
-/**
- * 리뷰등록 페이지
- */
-app.get("/add-review", (req, res) => {
-  app.locals.userId = mockUserId;
-  app.locals.addTagIds = new Set();
-  res.render("add-review");
-});
+  /**
+   * 리뷰등록 페이지
+   */
+  app.get("/add-review", (req, res) => {
+    app.locals.userId = mockUserId;
+    app.locals.addTagIds = new Set();
+    res.render("add-review");
+  });
 
-app.get("/add/tagList", (req, res) => {
-  const query = mybatisMapprer.getStatement(
-    "app",
-    "add/tagList",
-    {},
-    { language: "sql", indent: "  " }
-  );
-  connection.query(query, (err: MysqlError, rows: any) => {
+  app.get("/add/tagList", async (req, res) => {
+    const sql = bindSQL("add/tagList");
+    const [rows, fields] = await select(sql);
     res.render("components/tag-select", { tags: rows });
   });
-});
 
-app.post("/add/review", async (req, res) => {
-  const userId = app.locals.userId;
-  const { storeName, tagIds, reviewContent } = req.body;
-  const files = req.files as Express.Multer.File[];
-  const images = files.map((item) => item.buffer.toString("base64"));
+  app.post("/add/review", async (req, res, next) => {
+    const userId = app.locals.userId;
+    const { storeName, tagIds, reviewContent } = req.body;
+    const files = req.files as Express.Multer.File[];
+    const images = files.map((item) => item.buffer.toString("base64"));
 
-  console.log("==================inserted data==================");
-  // console.log(images);
-  console.log(JSON.stringify(req.body));
-  console.log("==================inserted data==================");
+    console.log("==================inserted data==================");
+    console.log(JSON.stringify(req.body));
+    console.log("==================inserted data==================");
 
-  if (!userId) {
-    res.send("세션정보가 없습니다.");
-    console.log("session expired");
-    return;
-  }
+    if (!userId) {
+      res.send("세션정보가 없습니다.");
+      console.log("session expired");
+      return;
+    }
 
-  connection.beginTransaction(() => {
-    const query1 = mybatisMapprer.getStatement(
-      "app",
-      "add/review/store",
-      { storeName },
-      { language: "sql", indent: "  " }
-    );
-    connection.query(query1, (err: MysqlError, rows: any) => {
-      const storeId = rows.insertId;
-      const query2 = mybatisMapprer.getStatement(
-        "app",
-        "add/review",
-        { storeId, userId, reviewContent },
-        { language: "sql", indent: "  " }
-      );
-      connection.query(query2, (err: MysqlError, rows: any) => {
-        const reviewId = rows.insertId;
-        const query3 = mybatisMapprer.getStatement(
-          "app",
-          "add/review/tag",
-          { reviewId, tagIds: tagIds.split(",") },
-          { language: "sql", indent: "  " }
-        );
-        connection.query(query3, (err, rows) => {
-          // connection.commit();
-          // console.log("rollback for btn test.---------------------");
-          // connection.rollback();
-          // res.send(true);
-        });
-        images.forEach((image, index) => {
-          const query4 = mybatisMapprer.getStatement(
-            "app",
-            "add/review/image",
-            { reviewId, index, image },
-            { language: "sql", indent: "  " }
-          );
-          connection.query(query4, (err, rows) => {
-            connection.commit();
-            // console.log("rollback for btn test.---------------------");
-            // connection.rollback();
-            // res.send(true);
-          });
-        });
+    await connection.beginTransaction();
+
+    try {
+      const sql1 = bindSQL("add/review/store", { storeName });
+      const [result1] = await insert(sql1);
+      const storeId = result1.insertId;
+
+      const sql2 = bindSQL("add/review", { storeId, userId, reviewContent });
+      const [result2] = await insert(sql2);
+      const reviewId = result2.insertId;
+
+      const sql3 = bindSQL("add/review/tag", { reviewId, tagIds: tagIds.split(",") });
+
+      const insertImages = images.map((image, index) => {
+        const sql4 = bindSQL("add/review/image", { reviewId, index, image });
+        return insert(sql4);
       });
-    });
+
+      await Promise.all([insert(sql3), ...insertImages]);
+
+      connection.commit();
+    } catch (e) {
+      connection.rollback();
+      next(e);
+    } finally {
+      connection.end();
+    }
+
+    res.send();
   });
-  res.send();
-});
+};
+start();
